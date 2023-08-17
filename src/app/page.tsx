d6c5fe26b8db9483eba648c6b20a9ba086457267
Vangnex <chengdidier@gmail.com>
2023-08-17 12:52:36 +0200

    UPDATE sidebar fixé et contenu non chevauchant avec le sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,144 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
+
     <main className={styles.main}>
       <nav className={styles.navigator}>
+        <Image
+          src="/didier.jpeg"
+          alt="Didier Photo"
+          className={styles.photo}
+          width={160}
+          height={160}
+        />
         <ul>
-          <li>link 1</li>
-          <li>link 2</li>
-          <li>link 3</li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>à propos</a>
+          </li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>expériences</a>
+          </li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>formations</a>
+          </li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>compétences</a>
+          </li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>portfolio</a>
+          </li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>intérêts</a>
+          </li>
+          <li className={styles.navli}>
+            <a className={styles.navlink}>promotions</a>
+          </li>
         </ul>
       </nav>
+      <core className={styles.core}>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+        <section>
+          <h2>Lorem Ipsum</h2>
+        </section>
+      </core>
 
     </main>
   )
